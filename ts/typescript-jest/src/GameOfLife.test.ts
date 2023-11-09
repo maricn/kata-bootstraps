2b4dbcbc8873cba67159f8b9c6b0e7cb0e739330
Nikola Marić <nikola.maric@personio.de>
2023-11-09 11:22:02 +0100

    feat: test should die when underpopulated

--- ts/typescript-jest/src/GameOfLife.test.ts
+++ ts/typescript-jest/src/GameOfLife.test.ts
@@ -8,4 +8,13 @@ describe("GameOfLife", () => {
 
     expect(cellStatus).toBe("ALIVE");
   });
+
+  it("should die when underpopulated", () => {
+    const gameOfLife = new GameOfLife();
+    const neighbors = { numOfDead: 2, numOfAlive: 1}
+
+    const cellStatus = gameOfLife.getCellStatus(neighbors);
+
+    expect(cellStatus).toBe("DEAD");
+  });
 });
